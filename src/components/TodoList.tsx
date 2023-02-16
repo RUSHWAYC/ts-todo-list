74f9248d6bb1d5a68ef1a8db7b2ac445c89f90ce
RUSHWAYC <rushwayc@yahoo.com>
2023-02-16 17:05:31 +0100

    Separated tasks in active and completed.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,15 +10,29 @@ interface Props {
 
 const TodoList = ({ todos, setTodos }: Props) => {
   return (
-    <div className="todos">
-      {todos.map((todo) => (
-        <SingleTodo
-          todo={todo}
-          key={todo.id}
-          todos={todos}
-          setTodos={setTodos}
-        />
-      ))}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      <div className="todos remove">
+        <span className="todos__heading">Completed tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            todos={todos}
+            key={todo.id}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
     </div>
   );
 };
